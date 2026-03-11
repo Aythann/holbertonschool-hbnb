ccbc2812fb0f0e4a824624b501f66a5503cb0ae6
Aythann <aythancristovao@gmail.com>
2026-03-11 15:54:23 +0100

    fix reviews api pycodestyle formatting

--- part3/hbnb/app/api/v1/reviews.py
+++ part3/hbnb/app/api/v1/reviews.py
@@ -7,9 +7,15 @@ api = Namespace("reviews", description="Review operations")
 review_model = api.model(
     "Review",
     {
-        "text": fields.String(required=True, description="Text of the review"),
-        "rating": fields.Integer(required=True, description="Rating of the place (1-5)"),
-        "place_id": fields.String(required=True, description="ID of the place"),
+        "text": fields.String(required=True, description="Text of review"),
+        "rating": fields.Integer(
+            required=True,
+            description="Rating of the place (1-5)"
+        ),
+        "place_id": fields.String(
+            required=True,
+            description="ID of the place"
+        ),
     },
 )
 
