5adc40141dd3085ab0452e936229b1c595645912
Aythann <aythancristovao@gmail.com>
2026-03-11 15:54:23 +0100

    fix base model pycodestyle line length

--- part3/hbnb/app/models/base_model.py
+++ part3/hbnb/app/models/base_model.py
@@ -6,8 +6,16 @@ from app import db
 class BaseModel(db.Model):
     __abstract__ = True
 
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id = db.Column(
+        db.String(36),
+        primary_key=True,
+        default=lambda: str(uuid.uuid4())
+    )
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
     updated_at = db.Column(
         db.DateTime,
         nullable=False,
